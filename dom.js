54644e3936d531d600cde3493e1e039f42a94617
SupriyaK31 <supriyakittur31@gmail.com>
2023-06-09 18:24:06 +0530

    getElementByTagName

--- dom.js
+++ dom.js
@@ -12,13 +12,23 @@
 
 //headertitle.style.borderBottom='solid 3px #000';
 
-var byclass=document.getElementsByClassName('list-group-item');
-//get element by classname
-console.log(byclass);
+// var byclass=document.getElementsByClassName('list-group-item');
+// //get element by classname
+// console.log(byclass);
 
-console.log(byclass[1]);
-byclass[2].style.backgroundColor='green'
-for(let i=0;i<byclass.length;i++){
-    byclass[i].style.fontWeight = 'bold'
+// console.log(byclass[1]);
+// byclass[2].style.backgroundColor='green'
+// for(let i=0;i<byclass.length;i++){
+//     byclass[i].style.fontWeight = 'bold'
+// }
+
+var li=document.getElementsByTagName('li');
+//get element by TagName
+
+console.log(li);
+li[2].style.backgroundColor='green'
+for(let i=0;i<li.length;i++){
+    li[i].style.fontWeight = 'bold'
 }
+
 
