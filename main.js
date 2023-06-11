d470107f88508e56fd33c7d97dd6978732e9eac6
SupriyaK31 <122157589+SupriyaK31@users.noreply.github.com>
2023-06-11 14:49:27 +0530

    Filter

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var form = document.getElementById('addForm');
 var itemList=document.getElementById('items');
+var filter=document.getElementById('filter');
 //add event listner
 form.addEventListener('submit', addItem);
 itemList.addEventListener('click', remoteItem);
+filter.addEventListener('keyup', searchFilter);
 //function call
 //EL
 function addItem(e){
@@ -32,4 +34,20 @@ function remoteItem(e){
           itemList.removeChild(li);
         }
     }
+}
+
+function searchFilter(e){
+    e.preventDefault();
+    //convert to lowercase
+    var text= e.target.value.toLowerCase();
+    //fatch li from itemlist
+    var items=itemList.getElementsByTagName('li');
+    Array.from(items).forEach(function(item){
+        var itemName= item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1){
+          item.style.display = "block";
+        }else{
+            item.style.display = "none";
+        }
+    });
 }
