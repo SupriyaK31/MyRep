d0b23d978672c2fedebb25bf4ddcc052298db79d
SupriyaK31 <122157589+SupriyaK31@users.noreply.github.com>
2023-06-11 12:22:42 +0530

    Add files via upload
    Event Listener

---
+++ main.js
@@ -0,0 +1,35 @@
+var form = document.getElementById('addForm');
+var itemList=document.getElementById('items');
+//add event listner
+form.addEventListener('submit', addItem);
+itemList.addEventListener('click', remoteItem);
+//function call
+
+function addItem(e){
+    e.preventDefault();
+    //save input text into newItem
+    var newItem= document.getElementById('item').value;
+    // create li element
+    var li= document.createElement('li');
+    // assign class to li
+    li.className="list-group-item"
+    // append text to li
+    li.appendChild(document.createTextNode(newItem));
+    //add btn to li
+     var delbtn = document.createElement('button');
+     delbtn.className="btn btn-danger btn-sm float-right delete";
+     delbtn.appendChild(document.createTextNode('x'));
+     li.appendChild(delbtn);
+    //append li to itemlist
+    itemList.appendChild(li);
+}
+function remoteItem(e){
+    e.preventDefault();
+    if(e.target.classList.contains('delete'))
+    {
+        if(confirm('Are u sure?')){
+           var li=e.target.parentElement;
+          itemList.removeChild(li);
+        }
+    }
+}
