0e7694bf983ba67d9b59ed2d6d311fb386eb6d7d
SupriyaK31 <122157589+SupriyaK31@users.noreply.github.com>
2023-06-15 21:46:01 +0530

    LocalStorage

--- demo.js
+++ demo.js
@@ -1,1 +1,20 @@
-console.log("I love Git")
+var form=document.getElementById('my-form');
+var inputName=document.getElementById('name');
+var inputEmail=document.getElementById('email');
+
+var userList= document.querySelector('#user')
+
+form.addEventListener('submit' , addUser);
+
+function addUser(e){
+    e.preventDefault();
+ if(inputName.value === '' || inputEmail.value === ''){
+    alert('Please enter all fields');
+ }  
+ else{
+   localStorage.setItem(typeof(`${inputName.value}`),`${inputEmail.value}`);
+    const li=document.createElement('li');
+    li.appendChild(document.createTextNode(`${inputName.value}   ${inputEmail.value}`));
+    userList.appendChild(li);
+ }
+}
