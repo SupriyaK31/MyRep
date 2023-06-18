652c5cbc68f2fdbf3f35e8443cd93eadab0a1ec9
SupriyaK31 <122157589+SupriyaK31@users.noreply.github.com>
2023-06-18 22:41:47 +0530

    Edit Btn LocalStorage

--- bookingData.js
+++ bookingData.js
@@ -17,26 +17,41 @@ function addUser(e){
       email:inputEmail.value,
       phone:inputPhone.value
    };
+   const li=document.createElement('li');
+   const btnDel=document.createElement('input');
+   const editBtn=document.createElement('input');
    let Serialized = JSON.stringify(userDetails);
    localStorage.setItem(inputName.value,Serialized);
-    const li=document.createElement('li');
+  
     let deselialied = JSON.parse(localStorage.getItem(inputName.value));
-    userList.appendChild(li);
     li.appendChild(document.createTextNode(`${inputName.value}- ${inputEmail.value}- ${inputPhone.value}`));
     let inputName1=inputName.value;
-    const btnDel=document.createElement('button');
-    btnDel.appendChild(document.createTextNode("Delete"));
-    btnDel.classList="btn btn-danger";
-    btnDel.addEventListener('click',delLocalstoarge);
-    
-    li.appendChild(btnDel);
-    
+    let emailInput1=inputEmail.value;
+    let numberInput1=inputPhone.value;
   
+    btnDel.appendChild(document.createTextNode("Delete"));
+    btnDel.classList="btn sm btn-danger";
+    btnDel.value="Delete";
+    btnDel.addEventListener('click',delLocalstoarge);
+    li.appendChild(btnDel);
+    li.appendChild(editBtn);
+    userList.appendChild(li);
     function delLocalstoarge(e){
       e.preventDefault();
      userList.removeChild(li);
       localStorage.removeItem(inputName1);
    }
+        editBtn.value="Edit";
+        editBtn.classList="btn btn-sm btn-secondary";
+        editBtn.addEventListener('click',onclickEdit);
+        function onclickEdit(e){
+            e.preventDefault();
+            inputName.value=inputName1;
+            inputEmail.value=emailInput1;
+            inputPhone.value=numberInput1;
+            userList.removeChild(li);
+            localStorage.removeItem(emailInput);
+        }
    inputName.value=" ";
    inputEmail.value=" ";
    inputPhone.value=" ";
