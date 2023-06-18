3eeacd51782de1b7047141a32cf136ab162b8e6c
SupriyaK31 <122157589+SupriyaK31@users.noreply.github.com>
2023-06-18 22:11:16 +0530

    DelBtnLocalStorage

--- bookingData.js
+++ bookingData.js
@@ -13,14 +13,33 @@ function addUser(e){
  }  
  else{
    let userDetails= {
-      name:`${inputName.value}`,
-      email:`${inputEmail.value}`,
-      phone:`${inputPhone.value}`
+      name:inputName.value,
+      email:inputEmail.value,
+      phone:inputPhone.value
    };
    let Serialized = JSON.stringify(userDetails);
-   localStorage.setItem(`${inputName.value}`,Serialized);
+   localStorage.setItem(inputName.value,Serialized);
     const li=document.createElement('li');
-    li.appendChild(document.createTextNode(`${inputName.value} ${inputEmail.value} ${inputPhone.value}`));
+    let deselialied = JSON.parse(localStorage.getItem(inputName.value));
     userList.appendChild(li);
+    li.appendChild(document.createTextNode(`${inputName.value}- ${inputEmail.value}- ${inputPhone.value}`));
+    let inputName1=inputName.value;
+    const btnDel=document.createElement('button');
+    btnDel.appendChild(document.createTextNode("Delete"));
+    btnDel.classList="btn btn-danger";
+    btnDel.addEventListener('click',delLocalstoarge);
+    
+    li.appendChild(btnDel);
+    
+  
+    function delLocalstoarge(e){
+      e.preventDefault();
+     userList.removeChild(li);
+      localStorage.removeItem(inputName1);
+   }
+   inputName.value=" ";
+   inputEmail.value=" ";
+   inputPhone.value=" ";
  }
 }
+
